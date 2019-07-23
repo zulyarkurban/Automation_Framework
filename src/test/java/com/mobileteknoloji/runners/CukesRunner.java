de4db259023b63c3a6d23398c3be8be37914ef0a
Zulyar Kurban <kurbanzulyar@gmail.com>
2019-07-23 15:21:18 -0400

    auth login test

--- src/test/java/com/mobileteknoloji/runners/CukesRunner.java
+++ src/test/java/com/mobileteknoloji/runners/CukesRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/mobileteknoloji/step_definitions"
         , dryRun = false
-        , tags = "@amazon"
+        , tags = "@positive"
 )
 public class CukesRunner {
 
